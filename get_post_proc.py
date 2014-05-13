90b1ad9ffeab62efff08675f440b04ff9f3c2ac1
Erik Rosolowsky <eros@bugaboo.westgrid.ca>
2014-05-13 10:32:40 -0700

    Split up template files by host name

--- get_post_proc.py
+++ get_post_proc.py
@@ -5,10 +5,11 @@ import shutil
 simnames = os.listdir('/global/scratch/eros/runs/SimSuite3/')
 steplist = list((21,22,23,24,25,26,27,28,29,30))
 facelist = list((0,1,2))
+un = os.uname()
 for nom in simnames:
     target_dir = '/global/scratch/eros/runs/'
     target_name = 'Radmc_{0}.pbs'.format(nom)
-    shutil.copy(os.getcwd()+'/templates/Radmc_template.pbs',target_dir+target_name)
+    shutil.copy(os.getcwd()+'/templates/Radmc_template_'+un[1]+'.pbs',target_dir+target_name)
     with open(target_dir+target_name,'a') as template:
         for face in facelist:
             for step in steplist:
