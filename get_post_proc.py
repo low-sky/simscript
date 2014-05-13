20375f2418bd967fc294560ed5d3c25482fcd651
Erik Rosolowsky <erosolow@ualberta.ca>
2014-05-12 22:34:44 -0700

    Radmc template scripts for designs on orcinus

---
+++ get_post_proc.py
@@ -0,0 +1,22 @@
+#!/bin/env python
+import os
+import shutil
+
+simnames = os.listdir('/global/scratch/eros/runs/SimSuite3/')
+steplist = list((21,22,23,24,25,26,27,28,29,30))
+steplist = list((21))
+facelist = list((0,1,2))
+for nom in simnames:
+    target_dir = '/global/scratch/eros/runs/'
+    target_name = 'Radmc_{0}.pbs'.format(nom)
+    shutil.copy(os.getcwd()+'/templates/Radmc_template.pbs',target_dir+target_name)
+    with open(target_dir+target_name,'a') as template:
+        for face in facelist:
+            for step in steplist:
+                template.write('$PPDIR/post_processing.sh $EHOME/runs/SimSuite3/'+nom+' {0} 128 {1}\n'.format(step,face))
+    template.close()
+
+
+
+
+    
