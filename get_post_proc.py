3108534a2cee5f62e5aa2844292d0558359b8cab
Erik Rosolowsky <erosolow@ualberta.ca>
2014-06-16 10:30:14 -0600

    Added design 5 and 6 components

--- get_post_proc.py
+++ get_post_proc.py
@@ -7,7 +7,8 @@ if len(sys.argv) == 1:
     try:
         #        simdir = os.listdir('/global/scratch/eros/runs/SimSuite3/')
         #        simdir = '/lustre/home/eros/runs/SimSuite4/'
-        simdir = '/lustre/home/eros/SimSuite5/'
+        #simdir = '/lustre/home/eros/SimSuite5/'
+        simdir = '/lustre/home/eros/SimSuite6/'
     except OSError:
         simdir = os.listdir('.')
 else:
@@ -27,7 +28,7 @@ for nom in simnames:
             target_dir ='./'
     else:
         target_dir = sys.argv[2]
-    target_name = 'Radmc_{0}.pbs'.format(nom)
+    target_name = 'Radmc6_{0}.pbs'.format(nom)
     shutil.copy(os.getcwd()+'/templates/Radmc_template_'+un[1]+'.pbs',target_dir+target_name)
     with open(target_dir+target_name,'a') as template:
         for face in facelist:
