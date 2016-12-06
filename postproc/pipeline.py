a1ccbd52170c00233b96d3b7e5ac0fe4ed728b9f
Erik Rosolowsky <erosolow@ualberta.ca>
2016-12-06 10:34:57 -0700

    pipeline tweaks

--- postproc/pipeline.py
+++ postproc/pipeline.py
@@ -12,18 +12,21 @@ level = float(sys.argv[4])
 ppdir = os.getenv('PPDIR')
 outdir = os.getenv('PPOUTDIR')
 #ppdir = '/home/eros/code/simscript/postproc/'
+
 D = pp.FileSetup(targetdir,face=face,level=level,timestep=timestep,ppdir=ppdir)
 
 #pp.ProblemSetup(D['FileName'], face = face, dust_temp = D['GasTemp'])
 
 os.chdir(D['TempDir'])
+
 command = ppdir+'radmc3d image npix '+str(int(D['GridSize']))+' iline 1 widthkms 10 linenlam 500 loadlambda fluxcons inclline linelist nostar writepop doppcatch sizepc 10 norefine'
 print(command)
 result = commands.getoutput(command)
 print(result)
 
-save_name = D['FileName'][:-5]+'_radmc.fits'
+save_name = '/lustre/home/eros/'+D['FileName'][17:-5]+'_radmc.fits'
 
 pp.MakeFits(fitsfile=save_name,dpc = 260.0,toK=True)
+
 shutil.move(save_name,outdir)
 shutil.rmtree(D['TempDir'])
